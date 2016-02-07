c8253d0c27b044cfd690984c4aba2272b201c0bf
Andrey Melnikov <andrey.melnikov@limelyte.com>
2016-02-07 11:34:41 -0800

    *Updated main to allow one argument. *Added check for number of queens passed in to not allow no-solution queens. *Made result output prettier

--- src/com/vafilor/algorithms/genetic/Main.java
+++ src/com/vafilor/algorithms/genetic/Main.java
@@ -7,22 +7,52 @@ public class Main {
 
     public static void main(String[] args)
     {
-        if(args.length != 2)
+        if(args.length < 1)
         {
-            System.out.println("Expected arguments: numberOfQueens startingPopulationSize");
+            System.out.println("Usage: <number of queens> [starting population size]");
             return;
         }
 
         int numberOfQueens = Integer.parseInt(args[0]);
-        int startingPopulationSize = Integer.parseInt(args[1]);
+
+        if(numberOfQueens < 1 || numberOfQueens == 2 || numberOfQueens == 3)
+        {
+            System.out.println("No solutions exist for that amount of queens");
+            return;
+        }
+
+        int startingPopulationSize = 8;
+
+        if(args.length > 1) {
+            startingPopulationSize = Integer.parseInt(args[1]);
+        }
 
         List<NQueensState> population = generateRandomNQueensStates(startingPopulationSize, numberOfQueens);
         IFitnessFunction<NQueensState> fitnessFunction = new NonAttackingQueensFunction(numberOfQueens);
 
         NQueensState result = GeneticAlgorithms.geneticAlgorithm(population, fitnessFunction, fitnessFunction.maxValue());
 
-        System.out.format("Fitness Score: %f %n", fitnessFunction.value(result) );
-        System.out.println(result);
+        printResults(result, fitnessFunction.value(result));
+    }
+
+    private static void printResults(NQueensState result, double fitnessScore)
+    {
+        System.out.format("Fitness Score: %f %n%n", fitnessScore );
+
+        System.out.format("%-5s Columns%n", "");
+
+        String columns = "";
+        String separator = "";
+
+        for(int i = 0; i < result.size(); i++)
+        {
+            columns += i + " ";
+            separator += "--";
+        }
+
+        System.out.format("%-5s %s%n", "", columns);
+        System.out.format("%-5s %s%n", "", separator);
+        System.out.format("%5s %s%n", "Row|", result);
     }
 
     private static List<NQueensState> generateRandomNQueensStates(int numberOfStates, int numberOfQueens)
