d401fe910fae28eee7c7e013d1a42920c1f9377e
Andrey Melnikov <vafilor@gmail.com>
2016-02-20 18:28:40 -0800

    Updated MisplaceTiles to use new SlidingBlockState and its methods

--- src/com/vafilor/search/MisplacedTiles.java
+++ src/com/vafilor/search/MisplacedTiles.java
@@ -1,7 +1,10 @@
 package com.vafilor.search;
 
 /**
+ * Created by Andrey Melnikov on 6/25/2015.
+ *
  * Evaluates cost to get to goal for a sliding block puzzle based on the number of misplaced tiles.
+ * Note: This assumes a 0 is first tile solution.
  *
  * E.g. (where 0 is blank)
  *
@@ -17,34 +20,20 @@ package com.vafilor.search;
  *
  * the cost would be: 8, as none of the tiles are in the proper spots.
  *
- * Created by Andrey Melnikov on 6/25/2015.
+ *
  */
-public class MisplacedTiles implements IHeuristic {
+public class MisplacedTiles implements IHeuristic<SlidingBlockState> {
 
-    /**
-     * Assumes the object passed in is an int[][]
-     * @param slidingBlockState
-     * @return
-     */
     @Override
-    //TODO for now this assumes zeroLast. Add zeroFirst version
-    public double getHeuristicCost(Object slidingBlockState) {
-        int[][] state = (int[][])slidingBlockState;
+    public double value(SlidingBlockState slidingBlockState) {
+        int[][] state = slidingBlockState.state;
+
         double misplaced = 0;
 
-        //TODO simplify
         for(int row = 0; row < state.length; row++) {
             for(int column = 0; column < state[row].length; column++) {
-
-                if((row == state.length - 1) && (column == state[row].length - 1)) {
-                    if(state[row][column] != 0) {
-                        misplaced++;
-                    }
-                } else {
-
-                    if (state[row][column] != (state.length * row + column + 1)) {
-                        misplaced++;
-                    }
+                if (state[row][column] != (state.length * row + column )) {
+                    misplaced++;
                 }
             }
         }
