67c9dc20f78df76d45a7fb2dbba234b9cea41499
Andrey Melnikov <vafilor@gmail.com>
2016-02-20 18:29:56 -0800

    Updated SlidingBlockProblem to use Generics and new classes, such as a TransitionModel to delegate logic.

--- src/com/vafilor/search/SlidingBlockProblem.java
+++ src/com/vafilor/search/SlidingBlockProblem.java
@@ -1,101 +1,63 @@
 package com.vafilor.search;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Andrey Melnikov on 6/7/2015.
  */
-public class SlidingBlockProblem implements ISearchProblem {
+public class SlidingBlockProblem<U> implements ISearchProblem<SlidingBlockState, U> {
 
-    private int length;
     private SlidingBlockState initialState;
-    private IHeuristic heuristic;
+    private ITransitionModel<SlidingBlockState, U> transitionModel;
+    protected ISlidingBlockGoal goalChecker;
 
-    public SlidingBlockProblem(int length) {
-        this.length = length;
-        this.initialState = new SlidingBlockState(this.length);
-        this.heuristic = new MisplacedTiles();
+    //TODO - null checks
+    public SlidingBlockProblem(ITransitionModel<SlidingBlockState, U> transitionModel, SlidingBlockState state, ISlidingBlockGoal goalChecker)
+    {
+        this.transitionModel = transitionModel;
+        this.initialState = state;
+        this.goalChecker = goalChecker;
     }
 
-    //TODO error catching
-    public SlidingBlockProblem(String sourceFile) throws FileNotFoundException {
-        this.initialState = new SlidingBlockState(sourceFile);
-        this.length = this.initialState.getLength();
-        this.heuristic = new MisplacedTiles(); //TODO consolidate constructors to not have to have this.heuristic intialized from both spots
+    public SlidingBlockProblem(ITransitionModel<SlidingBlockState, U> transitionModel, int length)
+    {
+        this(transitionModel, new SlidingBlockState(length), new ZeroFirstSlidingBlockGoal());
     }
 
     @Override
-    public Object getInitialState() {
+    public SlidingBlockState getInitialState() {
         return this.initialState;
     }
 
+
+    @Override
+    public SlidingBlockState getResultState(SlidingBlockState state, U action) {
+        return this.transitionModel.getResult(state, action);
+    }
+
+    @Override
+    public boolean isGoal(SlidingBlockState state) {
+        return this.goalChecker.isGoal(state);
+    }
+
     /**
-     * For this problem, we reduce possible actions by:
-     *  if the missing piece is at the left,top,right, or bottom, there is no need to include an action going the same way.
-     *  an action that would undo the previous is not included. E.g. I just moved down, so moving back up will not be included.
-     * @param node containing current problem state
-     * @return
+     * The step cost is the same for every move.
+     * @param state a state of the problem.
+     * @param action an action applicable to the state of the problem.
+     * @return 1.0
      */
     @Override
-    public List<Object> getPossibleActions(Object node) {
-        Node currentNode = (Node)node;
-        SlidingBlockState currentState = (SlidingBlockState)currentNode.getState();
-
-        SlidingAction previousAction = SlidingAction.Null;
-
-        if(currentNode.getAction() != null) {
-            previousAction = (SlidingAction)currentNode.getAction();
-        }
-
-        List<Object> possibleActions = new ArrayList<Object>();
-
-        if( !currentState.getBlankSpotPosition().isAtMinRow()  && !previousAction.equals(SlidingAction.Down) ) {
-            possibleActions.add(SlidingAction.Up);
-        }
-
-        if(!currentState.getBlankSpotPosition().isAtMaxRow()  && !previousAction.equals(SlidingAction.Up) ) {
-            possibleActions.add(SlidingAction.Down);
-        }
-
-        if(!currentState.getBlankSpotPosition().isAtMinColumn() && !previousAction.equals(SlidingAction.Right) ) {
-            possibleActions.add(SlidingAction.Left);
-        }
-
-        if(!currentState.getBlankSpotPosition().isAtMaxColumn() && !previousAction.equals(SlidingAction.Left) ) {
-            possibleActions.add(SlidingAction.Right);
-        }
-
-        return possibleActions;
-    }
-
-    @Override
-    public Object getResultingState(Object state, Object action) {
-
-        SlidingBlockState givenState = (SlidingBlockState)state;
-        SlidingAction givenAction = (SlidingAction)action;
-
-        return givenState.applyAction(givenAction);
-    }
-
-    @Override
-    public boolean isGoalState(Object state) {
-        SlidingBlockState givenState = (SlidingBlockState)state;
-
-        return givenState.isGoalState();
-    }
-
-    @Override
-    public double getStepCost(Object previousState, Object action) {
-        //For this problem, step cost is a sliding the block once. Path cost is total number of slides performed.
+    public double getStepCost(SlidingBlockState state, U action) {
         return 1.0;
     }
 
     @Override
-    public double getHeuristicCost(Object obj) {
-        SlidingBlockState state = (SlidingBlockState)obj;
+    public List<U> getPossibleActions(SlidingBlockState state) {
+        return this.transitionModel.getPossibleActions(state);
+    }
 
-        return this.heuristic.getHeuristicCost(state.getState());
+    @Override
+    public List<U> getPossibleActions(Node<SlidingBlockState, U> node) {
+        return this.transitionModel.getPossibleActions(node);
     }
 }
