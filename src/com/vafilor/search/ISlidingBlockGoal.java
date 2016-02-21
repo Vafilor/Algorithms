eeca0a5b5725d02e7acf34d34d1ea29e317a09d6
Andrey Melnikov <vafilor@gmail.com>
2016-02-20 18:28:13 -0800

    Updated ISlidingBlockGoal to use new SlidingBlockState methods

--- src/com/vafilor/search/ISlidingBlockGoal.java
+++ src/com/vafilor/search/ISlidingBlockGoal.java
@@ -1,14 +1,14 @@
 package com.vafilor.search;
 
 /**
- * Interface for objects that describe what a goal state is for a sliding block puzzle.
+ * Describe what a goal state is for a sliding block puzzle.
  *
  * Created by Andrey Melnikov on 6/25/2015.
  */
 public interface ISlidingBlockGoal {
     /**
-     * @param slidingBlockState assumes square array, or, the length of each row is the same.
+     * @param state
      * @return true if state is a goal, false otherwise.
      */
-    boolean isGoal(int[][] slidingBlockState);
+    boolean isGoal(SlidingBlockState state);
 }
