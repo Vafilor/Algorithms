d20d569efdc9dd3970196eda2925596c72b07cac
Andrey Melnikov <andrey.melnikov@limelyte.com>
2016-02-20 18:21:22 -0800

    Updated IHeuristic Interface to use generics and be more type safe

--- src/com/vafilor/search/IHeuristic.java
+++ src/com/vafilor/search/IHeuristic.java
@@ -1,11 +1,11 @@
 package com.vafilor.search;
 
 /**
- * Represents an object that calculates a heuristic function,
- * which is the estimated cost of the cheapest path from the state a Node contains to a goal state.
- *
  * Created by Andrey Melnikov on 6/25/2015.
+ *
+ * Represents an object that calculates a heuristic function for a particular state,
+ * which is the estimated cost of the cheapest path from the state to a goal state.
  */
-public interface IHeuristic {
-    double getHeuristicCost(Object node);
+public interface IHeuristic<T> {
+    double value(T state);
 }
