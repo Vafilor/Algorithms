f74d74fe21aca8cc66e54045ee2a267a7358f457
Andrey Melnikov <vafilor@gmail.com>
2016-02-20 18:27:46 -0800

    Updated ISearchProblem to use generics

--- src/com/vafilor/search/ISearchProblem.java
+++ src/com/vafilor/search/ISearchProblem.java
@@ -3,28 +3,55 @@ package com.vafilor.search;
 import java.util.List;
 
 /**
- * Created by Andrey Melnikov on 6/7/2015.
+ * Created by Andrey Melnikov on 2/12/2016
+ *
+ * Represents a Search Problem, which is a problem that can be broken down into states, with an initial state,
+ * and a goal state. The solution for a search problem is a sequence of actions that lead you from the initial state
+ * to the goal state. An example is the 8 puzzle.
+ *
+ * T is an object that represents a State of a problem.
+ * U is an object that represents an action applicable to a state of the problem.
  */
-
-//TODO look into more type-safe way to generalize a problem
-public interface ISearchProblem {
-    Object getInitialState();
-
+public interface ISearchProblem<T,U> {
     /**
-     * Node is passed in in case the user wants to limit possible actions based on previous actions.
-     * @param node containing current problem state
-     * @return a List of objects representing the possible actions for the problem
+     * @param state a state of a problem.
+     * @return all of the applicable actions in the given state.
      */
-    List<Object> getPossibleActions(Object node);
-    Object getResultingState(Object state, Object action);
-    boolean isGoalState(Object state);
-    double getStepCost(Object previousState, Object action);
+    List<U> getPossibleActions(T state);
 
     /**
-     * Represents a heuristic function.
+     * This method is passed in a Node so that the user can perform pruning of the actions based on previous actions already taken.
+     * For example, in the 8 puzzle, it makes no sense to go right if you just went left, this method would prune the right action as it
+     * is not productive to consider it.
      *
-     * @param state containing current problem state.
-     * @return the estimated cost of the cheapest path from the state to a goal state.
+     * @param node a node containing a state and references to a parent node.
+     * @return all of the applicable actions in the given state.
      */
-    double getHeuristicCost(Object state);
+    List<U> getPossibleActions(Node<T,U> node);
+
+    /**
+     * @param state a state of the problem.
+     * @param action an action applicable to the state of the problem.
+     * @return the state resulting from applying action to state.
+     */
+    T getResultState(T state, U action);
+
+    /**
+     * @param state a state of the problem.
+     * @return true if the state is a goal state, false otherwise.
+     */
+    boolean isGoal(T state);
+
+    /**
+     * @return the initial state of the problem.
+     */
+    T getInitialState();
+
+    /**
+     * A cost is a measure of "expense"/"badness". The higher the cost of a path to get to a state, the worse the path.
+     * @param state a state of the problem.
+     * @param action an action applicable to the state of the problem.
+     * @return the step cost of performing action in state.
+     */
+    double getStepCost(T state, U action);
 }
